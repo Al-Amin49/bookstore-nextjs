6e831453b0b6506d53d9f6f28a50d81e08a99a20
Al-Amin49 <alamin496832@gmail.com>
2024-08-14 15:44:46 +0600

    complete navbar

--- src/app/components/Shared/Navbar.tsx
+++ src/app/components/Shared/Navbar.tsx
@@ -1,17 +1,45 @@
+"use client"
+import { useState } from "react";
 import Link from "next/link";
-
+import { FaBars, FaTimes } from "react-icons/fa"; 
 
 const Navbar = () => {
-    return (
-        <div className="flex  items-center justify-around mx-2  bg-black text-white">
-            <h1 className="text-2xl font-medium text-green-500">Book Store</h1>
-            <nav className="space-x-5">
-                <Link href="/">Home</Link>
-                <Link href="/books">Books</Link>
-                <Link href="/addbook">Add Book</Link>
-            </nav>
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+   <>
+    <div className="bg-black text-white p-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-medium text-green-500">Book Store</h1>
+        <div className="md:hidden">
+          {/* Toggle button for mobile view */}
+          <button onClick={toggleMenu}>
+            {isOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </button>
         </div>
-    );
+        {/* Navigation links for larger screens */}
+        <nav className="space-x-5 hidden md:block">
+          <Link href="/">Home</Link>
+          <Link href="/books">Books</Link>
+          <Link href="/addbook">Add Book</Link>
+        </nav>
+      </div>
+     
+    </div>
+     {/* Navigation links for mobile view */}
+     {isOpen && (
+        <nav className=" space-y-3 md:hidden flex flex-col bg-black text-white w-fit rounded-md p-5 absolute right-0 top-20">
+          <Link href="/" onClick={toggleMenu}>Home</Link>
+          <Link href="/books" onClick={toggleMenu}>Books</Link>
+          <Link href="/addbook" onClick={toggleMenu}>Add Book</Link>
+        </nav>
+      )}
+   </>
+  );
 };
 
 export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import fs from "fs";
+import path from "path";
 
-export default function Home() {
-  return (
- <div>
+
+export default async function Home() {
+
+
+  const Home=async() =>{
+    const filePath = path.join(process.cwd(), 'data.json');
+    const jsonData = fs.readFileSync(filePath);
+    const books = JSON.parse(jsonData);
+    console.log(books)
+    return (
+   <div>
+    <h2>{books.title}</h2>
+   </div>
+    );
+  }
   
- </div>
-  );
+  
 }
