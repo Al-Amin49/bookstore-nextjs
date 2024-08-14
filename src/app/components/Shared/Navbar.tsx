0f0aac2439e8b80d0f85b2e63f4e61af31d07767
Al-Amin49 <alamin496832@gmail.com>
2024-08-14 13:07:11 +0600

    navbar layout created

--- src/app/components/Shared/Navbar.tsx
+++ src/app/components/Shared/Navbar.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <div className="flex  items-center justify-around m-2 ">
+        <div className="flex  items-center justify-around mx-2  bg-black text-white">
             <h1 className="text-2xl font-medium text-green-500">Book Store</h1>
-            <nav>
+            <nav className="space-x-5">
                 <Link href="/">Home</Link>
                 <Link href="/books">Books</Link>
                 <Link href="/addbook">Add Book</Link>
